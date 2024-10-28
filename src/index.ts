999f194c30142d086f47f4beaec8b8494bfb6587
ayamdobhal <me@iamdobhal.dev>
2024-10-28 07:49:48 +0530

    doc: add swagger

--- src/index.ts
+++ src/index.ts
@@ -1,9 +1,20 @@
 import cors from "@elysiajs/cors";
+import swagger from "@elysiajs/swagger";
 import { Elysia } from "elysia";
 import { optimalRoute } from "./routers/optimal-route";
 
 const app = new Elysia()
     .use(cors())
+    .use(
+        swagger({
+            documentation: {
+                info: {
+                    title: "Optimal Bridging Route API",
+                    version: "1.0.0",
+                },
+            },
+        }),
+    )
     .use(optimalRoute)
     .get("/", () => "Hello Elysia")
     .listen(3000);
