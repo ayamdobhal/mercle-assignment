d206e7644b8175dd284ea2d79b96783cef4cef00
ayamdobhal <me@iamdobhal.dev>
2024-10-28 06:42:43 +0530

    feat(user-balance): add error handling

--- src/helpers/user-balance.ts
+++ src/helpers/user-balance.ts
@@ -3,23 +3,27 @@ import { ERC20_ABI, PROVIDERS, USDC_ADDRESSES } from "../constants";
 
 export const getUserBalances = async (
     userAddress: string,
-    _tokenAddress: string, // not being used as of now due to hardcoded USDC token.
+    _tokenAddress: string, // not being used as of now due to hardcoded USDC address.
 ) => {
-    const balances = new Map<string, number>();
-    const balancePromises = Object.entries(PROVIDERS).map(
-        async ([chain, provider]) => {
-            const contract = new ethers.Contract(
-                USDC_ADDRESSES[chain],
-                ERC20_ABI,
-                provider,
-            );
-            const balanceWei = await contract.balanceOf(userAddress);
-            const balance = parseFloat(ethers.formatUnits(balanceWei, 18));
-            if (balance > 0) {
-                balances.set(chain, balance);
-            }
-        },
-    );
-    await Promise.all(balancePromises);
-    return balances;
+    const balances = new Map<number, number>();
+    try {
+        const balancePromises = Object.entries(PROVIDERS).map(
+            async ([chain, provider]) => {
+                const contract = new ethers.Contract(
+                    USDC_ADDRESSES[parseInt(chain)],
+                    ERC20_ABI,
+                    provider,
+                );
+                const balanceWei = await contract.balanceOf(userAddress);
+                const balance = parseFloat(ethers.formatUnits(balanceWei, 18));
+                if (balance > 0) {
+                    balances.set(parseInt(chain), balance);
+                }
+            },
+        );
+        await Promise.all(balancePromises);
+        return balances;
+    } catch (error: any) {
+        throw new Error(`Balances API Failed due to: ${error.message}`);
+    }
 };
